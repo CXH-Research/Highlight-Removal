b64eceb4e7ca14b2a04951642e90d3a80fadeecf
xuhangc <cxhhg11@gmail.com>
2024-09-15 23:08:03 +0800

    update

--- Yamamoto2019/Yamamoto2019.m
+++ Yamamoto2019/Yamamoto2019.m
@@ -1,15 +1,26 @@
 clc
 clear 
 close all
-Image_dir = '../EndoSRR';
+Image_dir = './EndoSTTN';
 listing = cat(1, dir(fullfile(Image_dir, '*.*g')));
 % The final output will be saved in this directory:
 result_dir = 'result';
 % Preparations for saving results.
 if ~exist(result_dir, 'dir'), mkdir(result_dir); end
 
-for i_img = 1:length(listing)
-    sfi = fix(im2double(imread(fullfile(Image_dir,listing(i_img).name))), @Shen2013);
+parfor i_img = 1:length(listing)
+    output_filename = fullfile(result_dir, listing(i_img).name);
+
+    % Check if the output file already exists
+    if exist(output_filename, 'file')
+        fprintf('Skipping %s (already exists in result folder)\n', listing(i_img).name);
+        continue;
+    end
+    try
+        sfi = fix(im2double(imread(fullfile(Image_dir,listing(i_img).name))), @Shen2013);
+    catch
+        sfi = fix(im2double(imread(fullfile(Image_dir,listing(i_img).name))), @Yoon2006);
+    end
     imwrite(sfi, fullfile(result_dir,listing(i_img).name));
 end
 
@@ -217,7 +228,7 @@ function I_d = fix(I, AuthorYEAR)
 
 if nargin == 2
     if isempty(AuthorYEAR)
-        AuthorYEAR = @Shen2013;
+        AuthorYEAR = @Yoon2006;
     else
         assert(isa(AuthorYEAR, 'function_handle'), 'SIHR:I:notTypeFunctionHandle', ...
             'Input AuthorYEAR is not of type function_handle.')
@@ -236,7 +247,7 @@ end
 
 % DRM: I = I_d + I_s
 I_d = feval(AuthorYEAR, I);
-I_s = my_clip(I-I_d, 0, 1);
+I_s = clip(I-I_d, 0, 1);
 
 I_d_m_1 = I_d;
 % I_d_init = I_d; % %DEBUGVAR%
@@ -251,12 +262,12 @@ max_iter_count = uint8(5);
 H_low = fspecial('average', 3);
 H_h_emph = -k * H_low;
 H_h_emph(2, 2) = 1 + k - k * H_low(2, 2);
-Theta = my_clip(imfilter(I, H_h_emph, 'symmetric'), 0, 1);
+Theta = clip(imfilter(I, H_h_emph, 'symmetric'), 0, 1);
 
 while true
-    Upsilon_d = my_clip(imfilter(I_d, H_h_emph, 'symmetric'), 0, 1);
-    Upsilon_s = my_clip(imfilter(I_s, H_h_emph, 'symmetric'), 0, 1);
-    Upsilon = my_clip(Upsilon_d+Upsilon_s, 0, 1);
+    Upsilon_d = clip(imfilter(I_d, H_h_emph, 'symmetric'), 0, 1);
+    Upsilon_s = clip(imfilter(I_s, H_h_emph, 'symmetric'), 0, 1);
+    Upsilon = clip(Upsilon_d+Upsilon_s, 0, 1);
 
     err_diff = sum(double(Upsilon_d > Theta), 3) >= 3; % 1
 
@@ -300,7 +311,7 @@ while true
 
     iter_count = iter_count + 1;
 
-    I_s = my_clip(I-I_d, 0, 1);
+    I_s = clip(I-I_d, 0, 1);
 
     if sqrt(immse(I_d, I_d_m_1)) < epsilon || ...
             iter_count >= max_iter_count
@@ -313,4 +324,74 @@ end
 % figure(1), imshow([I_d_init, I_d])
 % figure(2), imshow(10*abs(I_d-I_d_init))
 
+end
+
+function [Idiff] = Yoon2006(I)
+% figure(1), imshow(I)
+%% Create specular-free two-band image
+Isf = I - min(I,[],3);
+% figure(2), imshow(Isf)
+%% Get its chroma
+den = repmat(sum(Isf,3),[1 1 3]);
+zero = den==0;
+c = zeros(size(Isf));
+c(~zero) = Isf(~zero)./den(~zero);
+clear den zero
+%%
+% c = getChroma(Isf);
+cr = c(:,:,1);
+cg = c(:,:,2);
+%% Dimensions
+[nRow,nCol,nCh] = size(I);
+%% Reshape to column vector (easier indexing)
+I_col = reshape(I,[nRow*nCol nCh]);
+Isf_col = reshape(Isf,[nRow*nCol nCh]);
+cr_col = reshape(cr,[nRow*nCol 1]);
+cg_col = reshape(cg,[nRow*nCol 1]);
+skip = false([nRow*nCol 1]);
+%% Chroma threshold values (color discontinuity)
+thR = 0.05; thG = 0.05;
+%% Iterates until only diffuse pixels are left
+count = uint16(0);
+iter = uint16(0);
+while true
+    for x1 = 1:nRow*nCol-1
+        x2 = x1 + 1;
+        if skip(x1)
+            continue
+        elseif sum(Isf_col(x2),2) == 0 ||...
+                sum(I_col(x2),2) == 0 ||...
+                (abs(cr_col(x1)-cr_col(x2)) > thR &&...
+                abs(cg_col(x1)-cg_col(x2)) > thG)
+            skip(x1) = true;
+            continue
+        end
+        % Get local r_{d+s} ratio and r_{d}
+        rd = sum(Isf_col(x1),2)/sum(Isf_col(x2),2);
+        rds = sum(I_col(x1),2)/sum(I_col(x2),2);
+        % Compare ratios and decrease intensity
+        if rds > rd
+            m = sum(I_col(x1,2)) - rd*sum(I_col(x2,2));
+            if m < 1e-3
+                continue
+            end
+            I_col(x1,:) = max(0,I_col(x1,:) - m/3);
+            count = count + 1;
+        elseif rds < rd
+            m = sum(I_col(x2,2)) - sum(I_col(x1,2))/rd;
+            if m < 1e-3
+                continue
+            end
+            I_col(x2,:) = max(0,I_col(x2,:) - m/3);
+            count = count + 1;
+        end
+    end
+    if count == 0 || iter == 1000
+        break
+    end
+    count = 0;
+    iter = iter + 1;
+end
+%% Display diffuse image
+Idiff = reshape(I_col,[nRow nCol nCh]);
 end
