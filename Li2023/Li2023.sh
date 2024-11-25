f76774daa3cff3d2389918cccff9bfd4c7ac13ef
Nick Chen <cxhhg11@gmail.com>
2024-11-25 14:50:09 +0800

    Update Li2023.sh

--- Li2023/Li2023.sh
+++ Li2023/Li2023.sh
@@ -1,2 +1,2 @@
 # source /home/wsqlab/intel/oneapi/setvars.sh
-g++ -I /usr/include/eigen3 -I${MKLROOT}/include -fopenmp -o cmpb cmpb.cpp -L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl `pkg-config --cflags --libs opencv4`
+g++ -I /usr/include/eigen3 -I/opt/intel/oneapi/mkl/latest/include -fopenmp -o tmi tmi.cpp -L/opt/intel/oneapi/mkl/latest/lib/intel64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl `pkg-config --cflags --libs opencv4`
